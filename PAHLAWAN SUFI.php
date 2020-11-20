888ac12fb813e9cce071be517343bed583784b9b
ashrul <mohdashrul70@gmail.com>
2020-11-20 10:10:32 +0800

    update

---
+++ PAHLAWAN SUFI.php
@@ -0,0 +1,43 @@
+
+<!-- 
+***************************
+Date : 11 Septembet 2020  *
+Ditulis : ashrul          *
+*************************** 
+-->
+
+
+WARKAH WASIAT RAJA HAJI FI SABILILLAH UNTUK BANGSA MELAYU
+---------------------------------------------------------
+
+“Maka pada hari ini, tanggal 19 Rajab 1198 Hijrah bersamaan dengan 17 Jun 1784, 
+di Tanjung Palas Teluk Ketapang inilah warkah yang tertulis. Warkah ini kutulis 
+atas sejadah, mengadap kiblat dalam keadaan berwuduk setelah selesai solat dan 
+doa panjang untuk sekalian umat Melayu. Al-Qurán di sebelah kananku, kitab Dala-il 
+Khairat di sebelah kiriku. Badik pusaka di pinggangku. Sungguh demikian, aku amat 
+menyedari bahawa terdapat di kalangan armada perang Melayu yang aku satukan dan 
+pimpin ini terdapat golongan Melayu yang menjadi musuh dalam selimut: berwajah Melayu 
+berhati Belanda. Usahlah ditanya bagaimana aku mengetahuinya kerana tahu di angin turun naik. 
+
+Apabila Allah menghendaki terjadi sesuatu perkara, maka tiada siapa pun yang dapat
+mengelaknya. Biar sahaja mereka masuk menyelinap ke dalam kubu kami dan mengepung aku. 
+Syak fasik, yakin salih, batulah yang jadi juadah! Nah, wahai si munafik! Aku sesekali 
+tidak pernah takut kepada kalian. Marilah kita berdepan sebagai lelaki Melayu yang sejati. 
+Engkau kendonglah panji perjuanganmu. Aku junjung panji perjuanganku! Nyawa kita di tangan 
+Allah. Matiku di medan perang ini, tandanya sentiasalah aku hidup di sisi Allah. Matimu di 
+medan perang, tandanya celakalah engkau di Akhirat nanti. 
+
+Maka Maha Adillah Allah Taala sebagaimana terdapat golongan yang munafik dan petualang bangsa, 
+akan ada golongan di kalangan umat Melayu sesudah kami yang akan menyambung perjuangan kami. 
+Maka sambutlah panji perjuangan kami dengan penuh harga diri. Dalam hati merekalah aku hidup  
+dan menjadi cita-cita mereka pula mati kerana berjuang. Semangat mereka menyerupai kami, walaupun 
+bilangan mereka tidak seramai bilangan munafik dan petualang.”
+
+Sewaktu para pejuang yang setia bersama Raja Haji sedang mendirikan solat, pengkhianat bangsa 
+dan agama yang turut sama solat tersebut bangkit dari sujud tatkala imam dan jamaah lain masih 
+berada dalam sujud yang panjang, lalu mereka secara baculnya menancapkan pisau di belakang antara 
+mereka sehingga syahid.
+
+Raja Haji juga sempat menulis pada akhir warkahnya, sekiranya perjuangannya itu benar, semoga 
+Allah mengekalkan tulisannya itu dan membutakan mata orang Belanda daripada menjumpai warkah ini, 
+supaya warkahnya itu sampai kepada seluruh umat Melayu pada generasi mendatang."
